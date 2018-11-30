7fd0e1b953b15bef3a1070756f000547395cf393
Ichigo Kurosaki <42874053+JorgeFernandezJr@users.noreply.github.com>
2018-11-30 12:56:04 -0800

    Create oldhtml.py

---
+++ oldhtml.py
@@ -0,0 +1,85 @@
+<html><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8"><title> Computer Parts </title>
+<style>
+body{ margin:0;
+  background-color: #278221 ;
+  color: #000;
+  font-family: system-ui, sans-serif;
+  font-size: 24px;
+}
+</style>
+</head>
+<body>
+<div class="header">
+00000001  10110111  00000001  00000011
+</div>
+<pre><img src="/icons/blank.gif" alt="Icon "> <a href="?C=N;O=D">Name</a>                    <a href="?C=M;O=A">Last modified</a>      <a href="?C=S;O=A">Size</a>  <a href="?C=D;O=A">Description</a><hr><img src="/icons/folder.gif" alt="[DIR]"> <a href="code/">code/</a>                   2018-11-30 12:38    -   
+<img src="/icons/folder.gif" alt="[DIR]"> <a href="img/">img/</a>                    2018-11-30 09:22    -   
+<img src="/icons/folder.gif" alt="[DIR]"> <a href="logs/">logs/</a>                   2018-10-09 09:52    -   
+<img src="/icons/folder.gif" alt="[DIR]"> <a href="svg/">svg/</a>                    2018-11-19 09:32    -   
+<img src="/icons/image2.gif" alt="[IMG]"> <a href="asus-amd.png">asus-amd.png</a>            2018-11-30 09:21  4.8K  
+<img src="/icons/text.gif" alt="[TXT]"> <a href="computerpartgrid.html">computerpartgrid.html</a>   2018-11-30 09:23  567   
+<img src="/icons/text.gif" alt="[TXT]"> <a href="computerparts.html">computerparts.html</a>      2018-11-30 09:11  1.9K  
+<img src="/icons/text.gif" alt="[TXT]"> <a href="htaccess-rename.sh">htaccess-rename.sh</a>      2018-10-10 08:56  183   
+<img src="/icons/text.gif" alt="[TXT]"> <a href="url.html">url.html</a>                2018-10-11 09:12  1.2K  
+<hr></pre>
+a{
+  color: #fff ;font-size: 14px;background-color: #ffffff ;
+  text-decoration: none;
+}
+a:hover{
+	background-color: #93a1a1;
+}
+.list {
+position: absolute;
+top: 5%;
+left: 10%;
+font-family: system-ui , sans-serif;
+background-color: #be1f1f;
+color:  #24d0c8 ;
+border: solid 5px;
+border-style: solid;
+border-color: ##000000;
+border-radius: 10px;
+padding : 5px;
+}
+</style>
+</head>
+<body>
+<pre><div class="list">
+TYPE DATA BELOW THE ABOVE TAGS
+SAVE THE PAGE SOURCE OF THIS WEB PAGE AND EDIT IT.
+COMPUTER BIB - F LAST
+* * * * * * * * * * * * * * * * * * * * * * * * * *
+$1000 Computer
+$ ___ Motherboard with onboard video. (ATX or your choice.)
+$ ___ Processor (Intel or AMD)
+$ ___ Harddrve (SSD, M.2, PCIE or your choice.)
+$ ___ Harddrives for data (SATA)
+$ ___ Power supply.
+$ ___ RAM (16GB minimum)
+$ ___ CASE
+$ ___ TOTAL WITHOUT TAX
+LIST WHERE PARTS WHERE PURCHASED
+If you go over budget please documents the reason.
+* * * * * * * * * * * * * * * * * * * * * * * * * *
+$2500 Computer Newegg.com
+$ 95.78 ASUS TUF B350M-PLUS GAMING AM4 AMD B350 SATA 6Gb/s USB 3.1 HDMI Micro ATX AMD Motherboard
+$ 159.99 AMD RYZEN 5 1600 6-Core 3.2 GHz (3.6 GHz Turbo)
+$ 134.99 WD Blue 3D NAND 1TB PC SSD - SATA III 6 Gb/s M.2 2280 Solid State Drive
+$ ___ Harddrive for data (SATA)
+$ 54.99 Corsair Certified CP-9020053-NA RM Series RM550 - 550 Watt 80 PLUS Gold
+$ 159.99 CORSAIR Vengeance RGB Pro 16GB (2 x 8GB) 288-Pin DDR4 DRAM DDR4 3000 (PC4 24000) Desktop Memory Model 
+$ 89.00 LIAN LI ALPHA 330X Black SECC ATX Mid Tower Computer Case
+$ 649.00 MSI GeForce GTX 1080 DirectX.
+$ 1,443.73 TOTAL WITHOUT TAX
+LIST WHERE PARTS WHERE PURCHASED
+If you go over budget please documents the reason.
+* * * * * * * * * * * * * * * * * * * * * * * * * *
+DO NOT DELETE THE TAGS BELOW
+FEEL FREE TO CHNANGE COLORS
+</div>
+</pre>
+
+
+</body></html>
