531eedae6245407c66f09aba57b0d23c74fb81ab
Ichigo Kurosaki <42874053+JorgeFernandezJr@users.noreply.github.com>
2019-05-18 13:04:52 -0700

    Create camera.py

---
+++ code/camera.py
@@ -0,0 +1,17 @@
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera()
+
+// the code below is for recording
+camera.start_preview()
+camera.start_recording('/home/pi/video.h264')
+sleep(10)
+camera.stop_recording()
+camera.stop_preview()
+
+//the code below is for pictures
+camera.start_preview() 
+sleep(10)
+camera.capture('locations')
+camera.stop_preview()
