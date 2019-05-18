44afa27b41edfc7a7fb87be07bd805496cbd4616
Ichigo Kurosaki <42874053+JorgeFernandezJr@users.noreply.github.com>
2019-05-18 14:01:06 -0700

    Update camera.py

--- code/camera.py
+++ code/camera.py
@@ -16,3 +16,28 @@ camera.stop_preview()
 #sleep(10)
 #camera.capture('locations')
 #camera.stop_preview()
+
+
+#MATERIALS:
+#Raspberry Pi Zero Wireless × 1 
+#Amazon Affiliate Link - https://goo.gl/Zsxm7k
+
+#Raspberry Pi Camera module × 1
+#Amazon Affiliate Link - https://goo.gl/4kR632
+
+#Check out the NoIR camera for night surveillance (not needed)
+#Amazon Affiliate Link - https://goo.gl/fT7oL9
+
+#Raspberry Pi Zero Camera Cable x 1
+#Amazon Affiliate Link - https://goo.gl/pBVZb1
+ 
+#Micro-USB to USB Cable × 1 
+#1/4" MDF Board × 1 
+#90 Degree Angle Bracket × 1 
+#Outdoor Mounting Tape × 1 
+#M4 X 20mm Long Machine Screws × 2 
+#M4 Hex Nuts × 2 
+#M2.5 Machine Screws × 4 
+#8 X 3/4" Long Machine Screws × 1 
+#8 Hex Nut × 1 
+#8 Wing Nut × 1 
