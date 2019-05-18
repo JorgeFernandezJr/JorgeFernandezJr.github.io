0b79d5baa656a8b07f048ef46a745e5045d45857
Ichigo Kurosaki <42874053+JorgeFernandezJr@users.noreply.github.com>
2019-05-18 13:06:17 -0700

    Update camera.py

--- code/camera.py
+++ code/camera.py
@@ -3,15 +3,15 @@ from time import sleep
 
 camera = PiCamera()
 
-// the code below is for recording
+# the code below is for recording
 camera.start_preview()
 camera.start_recording('/home/pi/video.h264')
 sleep(10)
 camera.stop_recording()
 camera.stop_preview()
 
-//the code below is for pictures
-camera.start_preview() 
-sleep(10)
-camera.capture('locations')
-camera.stop_preview()
+# the code below is for pictures (comment out one section to use the other [//] [#])
+#camera.start_preview() 
+#sleep(10)
+#camera.capture('locations')
+#camera.stop_preview()
